c08d7975697f9bb127790e6459c62ab8578ab0ae
Ruslan <kros8925@gmail.com>
2023-09-21 18:32:24 +0300

    Add TTL and expiresAt field for Item

--- item.go
+++ item.go
@@ -1,5 +1,34 @@
 package cache
 
+import "time"
+
+const (
+	ItemNoTTL      time.Duration = -1
+	ItemDefaultTTL time.Duration = 0
+)
+
 type Item struct {
-	Value interface{}
+	Value     interface{}
+	TTL       time.Duration
+	ExpiresAt time.Time
+}
+
+func newItem(value interface{}, ttl time.Duration) Item {
+	expiresAt := time.Now().Add(ttl)
+
+	item := Item{
+		Value:     value,
+		TTL:       ttl,
+		ExpiresAt: expiresAt,
+	}
+
+	return item
+}
+
+func (i Item) Expired() bool {
+	if i.TTL <= 0 {
+		return false
+	}
+
+	return time.Now().After(i.ExpiresAt)
 }
