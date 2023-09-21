65471e8df0d3fe30daedf49bb16a30c6f735acfd
Ruslan <kros8925@gmail.com>
2023-09-21 18:33:02 +0300

    refactor metrics and add ttl

--- cache.go
+++ cache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"sync"
+	"time"
 )
 
 type Cache struct {
@@ -26,13 +27,39 @@ func New(opts ...optionsFunc) *Cache {
 		options: options,
 		metrics: NewMetrics(),
 	}
-
 }
 
 // Set the key to hold a value.
 // If key already holds a value, It will be overwritten.
 func (c *Cache) Set(key string, value interface{}) {
-	c.set(key, value)
+	ttl := c.options.ttl
+
+	c.set(key, value, ttl)
+}
+
+// Similar to Set method, but with an opportunity to adjust a ttl for that
+// particular key manually.
+func (c *Cache) SetWithTTL(key string, value interface{}, ttl time.Duration) {
+	c.set(key, value, ttl)
+}
+
+// Set the key to hold a value, and then returns it.
+func (c *Cache) SetGet(key string, value interface{}) interface{} {
+	ttl := c.options.ttl
+
+	c.set(key, value, ttl)
+	v := c.get(key)
+
+	return v
+}
+
+// Similar to SetGet method, but with an opportunity to adjust  ttl for that
+// particular key manually.
+func (c *Cache) SetGetWithTTL(key string, value interface{}, ttl time.Duration) interface{} {
+	c.set(key, value, ttl)
+	v := c.get(key)
+
+	return v
 }
 
 // Get the value of key.
@@ -56,8 +83,10 @@ func (c *Cache) GetMultiple(keys []string) []interface{} {
 // Get the old value stored by key and set the new one for that key.
 // If the key doesn't exist, nil value will be returned.
 func (c *Cache) GetSet(key string, value interface{}) interface{} {
+	ttl := c.options.ttl
+
 	v := c.get(key)
-	c.set(key, value)
+	c.set(key, value, ttl)
 
 	return v
 }
@@ -103,8 +132,8 @@ func (c *Cache) Keys() []string {
 	return keys
 }
 
-// Get count of keys in the cache.
-func (c *Cache) KeysCount() int {
+// Count returns the number of stored elements in the cache.
+func (c *Cache) Count() int {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
@@ -130,35 +159,34 @@ func (c *Cache) Metrics() *Metrics {
 	return c.metrics
 }
 
-func (c *Cache) set(key string, value interface{}) {
+func (c *Cache) set(key string, value interface{}, ttl time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	item := Item{
-		Value: value,
-	}
+	item := newItem(value, ttl)
 	c.items[key] = item
 
-	if c.options.enableMetrics {
-		c.metrics.incrInsertions()
-	}
+	c.metricsIncrInsertions()
 }
 
 func (c *Cache) get(key string) interface{} {
 	c.mu.RLock()
 	defer c.mu.RUnlock()
 
-	value := c.items[key].Value
+	item := c.items[key]
+	value := item.Value
+
 	if value == nil {
-		if c.options.enableMetrics {
-			c.metrics.incrMisses()
-		}
+		c.metricsIncrMisses()
 		return nil
 	}
 
-	if c.options.enableMetrics {
-		c.metrics.incrHits()
+	if item.Expired() {
+		c.metricsIncrMisses()
+		return nil
 	}
+
+	c.metricsIncrHits()
 
 	return value
 }
@@ -168,4 +196,22 @@ func (c *Cache) delete(key string) {
 	defer c.mu.Unlock()
 
 	delete(c.items, key)
+}
+
+func (c *Cache) metricsIncrInsertions() {
+	if c.options.enableMetrics {
+		c.metrics.incrInsertions()
+	}
+}
+
+func (c *Cache) metricsIncrHits() {
+	if c.options.enableMetrics {
+		c.metrics.incrHits()
+	}
+}
+
+func (c *Cache) metricsIncrMisses() {
+	if c.options.enableMetrics {
+		c.metrics.incrMisses()
+	}
 }
