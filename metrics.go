ce08297d588c1c2f73415021a754994f8ed845f0
Ruslan <kros8925@gmail.com>
2023-09-21 18:32:45 +0300

    Add evictions to metrics

--- metrics.go
+++ metrics.go
@@ -3,33 +3,41 @@ package cache
 import "sync/atomic"
 
 type Metrics struct {
-	// Show how many times items were inserted into cache
+	// Shows how many times items were inserted into cache.
 	insertions uint64
 
-	// Show how many times items were successfully retrived by key
+	// Shows how many times items were successfully retrived by key.
 	hits uint64
 
-	// Show how many times items weren't retrived by key
+	// Shows how many times items weren't retrived by key.
 	misses uint64
+
+	// Shows how many items were released from the cache.
+	evictions uint64
 }
 
 func NewMetrics() *Metrics {
 	return &Metrics{}
 }
 
-// Get collected insertions
+// Get collected insertions.
 func (m *Metrics) Insertions() uint64 {
 	return atomic.LoadUint64(&m.insertions)
 }
 
-// Get collected hits
+// Get collected hits.
 func (m *Metrics) Hits() uint64 {
 	return atomic.LoadUint64(&m.hits)
 }
 
-// Get collected misses
+// Get collected misses.
 func (m *Metrics) Misses() uint64 {
 	return atomic.LoadUint64(&m.misses)
+}
+
+// Get collected evictions.
+func (m *Metrics) Evictions() uint64 {
+	return atomic.LoadUint64(&m.evictions)
 }
 
 func (m *Metrics) incrInsertions() {
@@ -42,4 +50,8 @@ func (m *Metrics) incrHits() {
 
 func (m *Metrics) incrMisses() {
 	atomic.AddUint64(&m.misses, 1)
+}
+
+func (m *Metrics) incrEvictions() {
+	atomic.AddUint64(&m.evictions, 1)
 }
